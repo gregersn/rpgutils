cac6e36e3afac4622b2ae5adfe949d2bfa4ca8a8
Greger Stolt Nilsen <gregersn@gmail.com>
2021-03-27 19:59:43 +0100

    Show result of separate dice, limit number of dice

--- rpgutils/dice.py
+++ rpgutils/dice.py
@@ -10,6 +10,19 @@ class Result:
     rolls: List[int]
     modifier: int
 
+    def __str__(self):
+        modifier = ''
+        if self.modifier != 0:
+            modifier = f'{"+" if self.modifier > 0 else "-"}{abs(self.modifier)}'
+
+        if len(self.rolls) > 1:
+            return f"[{' + '.join(str(r) for r in self.rolls)}]{modifier} = {self.total}"
+
+        if self.modifier != 0:
+            return f"{self.rolls[0]}{modifier} = {self.total}"
+
+        return f"{self.total}"
+
 
 class Die():
     count: int
@@ -45,9 +58,11 @@ class Roll():
         self._dice = []
 
     def __str__(self):
-        return " ".join([self.dice(), "giving", str(self.roll())])
+        return " ".join([self.dice(), "giving", ", ".join(self.roll())])
 
     def roll(self):
+        results = [str(d.roll()) for d in self._dice]
+        return results
         return sum(d.roll().total for d in self._dice)
 
     def advantage(self):
@@ -66,7 +81,7 @@ class Roll():
         return len(self._dice)
 
 
-DICE_MATCH = r'(\d+)?d(\d+)([\+\-]\d+)?'
+DICE_MATCH = r'(\d+)?[dD](\d+)([\+\-]\d+)?'
 
 
 def roll(commands: str = ""):
@@ -87,6 +102,9 @@ def roll(commands: str = ""):
             quantity = int(match.group(1)) if match.group(1) else 1
             die = int(match.group(2)) if match.group(2) else 0
             modifier = int(match.group(3)) if match.group(3) else 0
+
+            if quantity > 100:
+                return "too many dice!"
             roll.add_die(die, quantity, modifier)
 
     if roll.count() < 1:
@@ -103,6 +121,7 @@ def main():
     print(roll("+"))
     print(roll("-"))
     print(roll("d4+1"))
+    print(roll("100d4"))
 
 
 if __name__ == '__main__':
